a41f6484ac21ce89b7ad5fd5dce52bc33060549f
Matheus Muriel <matheus.muriel@outlook.com>
2018-09-26 22:21:54 -0300

    Entrando Na Porta

---
+++ Assets/scripts/EntrarNaPorta.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntrarNaPorta : MonoBehaviour {
+    public GameObject objetoGatilho;
+    public GameObject objetoAtivador;
+    public float rangeDeAtivacao;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 objA = objetoAtivador.transform.position; //Posicao do personagem
+        Vector2 objB = objetoGatilho.transform.position;  //Posicao do gatilho
+
+
+        if (Vector2.Distance(objA, objB) <= rangeDeAtivacao)
+        {
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                Application.LoadLevel("dentroCasa");
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Assets/scripts/MensagemPorta.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MensagemPorta : MonoBehaviour {
+    public float tempoDaMensagem;
+    public Text texto;
+    public GameObject objetoGatilho;
+    public GameObject objetoAtivador;
+    public float rangeDeAtivacao;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 objA = objetoAtivador.transform.position; //Posicao do personagem
+        Vector2 objB = objetoGatilho.transform.position;  //Posicao do gatilho
+
+
+        if (Vector2.Distance(objA, objB) <= rangeDeAtivacao)
+        {
+            texto.enabled = true;
+        }
+        else
+        {
+            texto.enabled = false;
+        }
+    }
+}
